7ed7c2f9d6d456d4a142d9d87445030d6f898cd5
Daniel Valdivieso-M <dvaldi7@gmail.com>
2025-09-26 00:11:17 +0200

    added createFighters to fighters.controllers

--- controllers/fighters.controllers.js
+++ controllers/fighters.controllers.js
@@ -4,9 +4,9 @@ const pool = require("../config/db");
 const getFighters = async (req, res) => {
     try {
         const [fighters] = await pool.query('SELECT * FROM fighters');
-        res.json(fighters); 
+        res.json(fighters);
 
-    } catch (err) { 
+    } catch (err) {
         console.error("Error al obtener los peleadores", err)
         res.status(500).json({
             error: "Error al obtener luchadores",
@@ -15,7 +15,56 @@ const getFighters = async (req, res) => {
 };
 
 // Crear nuevo peleador
+const createFighters = async (req, res) => {
+    const { 
+        first_name, 
+        last_name, 
+        nickname, 
+        record_wins, 
+        record_losses, 
+        record_draws, 
+        weight_class 
+    } = req.body;
+
+   if (!first_name || !last_name || !weight_class) {
+        return res.status(400).json({
+            status: "error",
+            message: "Faltan algunos campos obligatorios (nombre, apellido o apodo)",
+        });
+    }
+
+    try {
+        const [result] = await pool.query(
+                    `INSERT INTO fighters 
+            (first_name, last_name, nickname, record_wins, record_losses, record_draws, weight_class) 
+            VALUES (?, ?, ?, ?, ?, ?, ?)`,
+            [
+                first_name, 
+                last_name, 
+                nickname, 
+                record_wins || 0, 
+                record_losses || 0, 
+                record_draws || 0, 
+                weight_class
+            ]
+        );
+        
+                res.status(201).json({
+                    message: 'Peleador creado con éxito',
+                    fighterId: result.insertId,
+                    data: req.body
+                });
+    
+    } catch (error) {
+        console.error("Error al crear el peleador: ", error);
+        res.status(500).json({
+            status: "error",
+            message: "Error al crear el peleador",
+        })
+    }
+}
 
 module.exports = {
     getFighters,
+    createFighters,
 }
--- routes/fighters.routes.js
+++ routes/fighters.routes.js
@@ -6,6 +6,7 @@ const fightersController = require("../controllers/fighters.controllers");
 
 //Rutas
 router.get('/', fightersController.getFighters);
+router.post('/', fightersController.createFighters);
 
 // Exportamos el router para usarlo en index.js
 module.exports = router;
