45176fd8b2ce8e06b4d816ccccd8971662ec1d0f
Daniel Valdivieso-M <dvaldi7@gmail.com>
2025-12-29 00:55:03 +0100

    added news.controllers

---
+++ controllers/news.controllers.js
@@ -0,0 +1,78 @@
+const pool = require('../config/db');
+
+
+const getNews = async (req, res) => {
+
+    try {
+        const [rows] = await pool.query('SELECT * FROM news ORDER BY published_at DESC');
+
+        res.json(rows);
+
+    } catch (error) {
+        res.status(500).json({
+            status: "error",
+            message: "Error al obtener las noticias",
+        })
+    }
+}
+
+const createNews = async (req, res) => {
+
+const { title, content } = req.body;
+
+const titleForSlug = title;
+const slug = slugify(titleForSlug, { lower: true, strict: true });
+
+let image_url = null;
+    if (req.file) {
+        image_url = `images/news/${req.file.filename}`;
+    }
+
+    try {
+        const sqlQuery = `
+            INSERT INTO news (
+                news_id, title, content, image_url, slug, published_at 
+            ) 
+            VALUES (?, ?, ?, ?, ?, ? )
+        `;
+        const [ result ] = await pool.query(sqlQuery, [
+            news_id, 
+            title, 
+            content, 
+            image_url, 
+            slug, 
+            published_at 
+        ]);
+
+        res.status(201).json({
+            status: "success",
+            message: "Noticia creada con éxito",
+            news_id: result.insertId,
+            image_url: image_url,
+            slug: slug,
+        })
+
+    } catch (error) {
+        // Manejo específico para slugs duplicados
+        if (error.code === 'ER_DUP_ENTRY') {
+            // Si el slug ya existe, le añadimos un timestamp para que sea único
+            slug = `${slug}-${Date.now()}`;
+            return res.status(400).json({ 
+                message: "Ya existe una noticia con un título similar, por favor cámbialo un poco." 
+            });
+        }
+        console.error("Error: ", error);
+        res.status(500).json({
+            status: "error",
+            message: "error al crear la noticia",
+        })
+    }
+}
+
+
+
+
+module.exports = {
+    getNews,
+    createNews,
+}
