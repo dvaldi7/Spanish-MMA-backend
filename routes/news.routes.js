e69ba99a13863e3257393d9decb7c1e6c01f87d5
Daniel Valdivieso-M <dvaldi7@gmail.com>
2025-12-31 15:58:31 +0100

    added deleteFighter route

--- routes/news.routes.js
+++ routes/news.routes.js
@@ -11,6 +11,7 @@ router.get('/slug/:slug', newsController.getNewsBySlug);
 
 // Rutas privadas
 router.post('/', auth(['admin']), upload.single('image'), newsController.createNews);
+router.delete('/:id',auth(['admin']), newsController.deleteNews);
 
 
 
