d62b9c19523f2bb15cb8cce760da40d325aedae7
Daniel Valdivieso-M <dvaldi7@gmail.com>
2026-02-03 11:02:11 +0100

    added new fighters.route for verification token in front

--- routes/fighters.routes.js
+++ routes/fighters.routes.js
@@ -10,10 +10,21 @@ router.get('/', fightersController.getFighters);
 router.get('/id/:id', fightersController.getFightersById);
 router.get('/slug/:slug', fightersController.getFightersBySlug);
 
+
+//Ruta para verificación del token
+router.get('/auth/verify', auth(['admin']), (req, res) => {
+    res.status(200).json({ 
+        status: "success", 
+        message: "Sesión válida",
+        user: req.user 
+    });
+});
+
 // Rutas privadas
 router.post('/', auth(['admin']), upload.single('photo'), fightersController.createFighters);
 router.put('/id/:id', auth(['admin']), upload.single('photo'), fightersController.updateFighter);
 router.put('/assign/:fighterId/company/:companyId', auth(['admin']), fightersController.assignFighterToCompany);
 router.delete('/id/:id', auth(['admin']), fightersController.deleteFighter);
+
 
 module.exports = router;
